da634495394e023b9879c70d70551898f6a3e701
UrbanPistek <urban.pistek@gmail.com>
2022-10-19 17:23:36 -0400

    wip - created script for mlp, need to add metrics and cleanup

--- multi-layer-perceptron/jax/main.py
+++ multi-layer-perceptron/jax/main.py
@@ -3,17 +3,73 @@ import time
 import logging
 import jax
 import mlp
+import jax.numpy as jnp
+import tensorflow_datasets as tfds
 
-# Suppress warning and info messages from jax 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# Suppress warning, info and error messages from jax, tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Specify directory for data 
+DATA_DIR = './datasets'
+
+def load_datasets():
+
+    # Fetch full datasets for evaluation
+    # tfds.load returns tf.Tensors (or tf.data.Datasets if batch_size != -1)
+    # You can convert them to NumPy arrays (or iterables of NumPy arrays) with tfds.dataset_as_numpy
+    mnist_data, info = tfds.load(name="mnist", batch_size=-1, data_dir=DATA_DIR, with_info=True)
+
+    mnist_data = tfds.as_numpy(mnist_data)
+    train_data, test_data = mnist_data['train'], mnist_data['test']
+    num_labels = info.features['label'].num_classes
+    h, w, c = info.features['image'].shape
+    num_pixels = h * w * c
+
+    # # Full train set
+    train_images, train_labels = train_data['image'], train_data['label']
+    train_images = jnp.reshape(train_images, (len(train_images), num_pixels))
+    train_labels = mlp.one_hot_encoder(train_labels, num_labels)
+
+    # # Full test set
+    test_images, test_labels = test_data['image'], test_data['label']
+    test_images = jnp.reshape(test_images, (len(test_images), num_pixels))
+    test_labels = mlp.one_hot_encoder(test_labels, num_labels)
+
+    print('Train:', train_images.shape, train_labels.shape)
+    print('Test:', test_images.shape, test_labels.shape)
+
+    return train_images, train_labels, test_images, test_labels, num_labels, num_pixels
+
+def get_train_batches(batch_size):
+    
+    # as_supervised=True gives us the (image, label) as a tuple instead of a dict
+    ds = tfds.load(name='mnist', split='train', as_supervised=True, data_dir=DATA_DIR)
+    
+    # You can build up an arbitrary tf.data input pipeline
+    ds = ds.batch(batch_size).prefetch(1)
+    
+    # tfds.dataset_as_numpy converts the tf.data.Dataset into an iterable of NumPy arrays
+    return tfds.as_numpy(ds)
 
 def main():
 
     print(f"\nEnvironment Config: ")
     print(f"TF_CPP_MIN_LOG_LEVEL = {os.environ['TF_CPP_MIN_LOG_LEVEL']}")
 
+    # Initialize model
     layers = [784, 100, 10]
-    model = mlp.MLPModel(layers, learning_rate=0.01, log_level=logging.INFO)
+    batch_size = 128
+    epochs = 10
+    n_targets = 10
+    learning_rate = 0.01
+    model = mlp.MLPModel(layers, learning_rate=learning_rate, batch_size=batch_size, num_epochs=epochs, n_targets=n_targets)
+
+    # Load Data 
+    train_images, train_labels, test_images, test_labels, num_labels, num_pixels = load_datasets()
+    ds = get_train_batches(batch_size)
+
+    # Train model
+    mlp.train(model.params, ds, train_images, train_labels, test_images, test_labels, num_labels, num_pixels, learning_rate, epochs=epochs)
 
 if __name__ == "__main__":
     main()
--- multi-layer-perceptron/jax/mlp.py
+++ multi-layer-perceptron/jax/mlp.py
@@ -1,12 +1,12 @@
+import time
 import jax.numpy as jnp
 from jax import grad, jit, vmap
 from jax import random
 from jax.scipy.special import logsumexp
-import logging
 
 class MLPModel:
 
-    def __init__(self, layer_sizes, learning_rate=0.01, log_level=logging.DEBUG):
+    def __init__(self, layer_sizes, num_epochs=1, batch_size=128, n_targets=10, learning_rate=0.01):
 
         # Array of each layer size
         self.layer_sizes = layer_sizes
@@ -15,16 +15,13 @@ class MLPModel:
         # Scaling apply to estimated weight error during backpropagation
         self.learning_rate = learning_rate
 
-        # Configure logging 
-        self.log_level =  log_level
-        self.logger = logging.getLogger("mlp-logger")
-        # logging.basicConfig(level=self.log_level)
+        # Configure parameters
+        self.num_epochs = num_epochs
+        self.batch_size = batch_size
+        self.n_targets = n_targets
 
         # Initialize all layers with random values
-        self.__init_layers
-
-        self.logger.debug("Layers Initialized:")
-        print(f"Layers Initialized: {self.layer_sizes}")
+        self.params = self.__init_layers()
 
     def __init_layer_params(self, m, n, key, scale=1e-2):
         """
@@ -32,12 +29,95 @@ class MLPModel:
         for a dense neural network layer.
         """
         w_key, b_key = random.split(key)
+        
         return scale * random.normal(w_key, (n, m)), scale * random.normal(b_key, (n,))
 
     def __init_layers(self, key=random.PRNGKey(0) ):
         """
         Initialize all layers for a fully-connected neural network.
         """
-        keys = random.split(key, len(sizes))
         sizes = self.layer_sizes
-        self.params =  [self.__init_layer_params(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+        keys = random.split(key, len(sizes))
+
+        return [self.__init_layer_params(m, n, k) for m, n, k in zip(sizes[:-1], sizes[1:], keys)]
+
+def relu(x):
+    """
+    Rectified Linear Operation.
+    """
+    return jnp.maximum(0, x)
+
+def one_hot_encoder(x, k, dtype=jnp.float32):
+    """
+    Create a one-hot encoding of x of size k.
+    """
+    return jnp.array(x[:, None] == jnp.arange(k), dtype)
+
+def predict(params, input):
+    """
+    Run a single prediction on the input. 
+
+    Parameters: 
+        input (array): A n-dimensional array, typically a 1D or 2D array. For images for instance this is a 2D array. 
+    """
+
+    activations = input
+    for w, b in params[:-1]:
+        outputs = jnp.dot(w, activations) + b
+        activations = relu(outputs)
+    
+    final_w, final_b = params[-1]
+    logits = jnp.dot(final_w, activations) + final_b
+    
+    return logits - logsumexp(logits)
+
+# Temp, move this
+batched_predict = vmap(predict, in_axes=(None, 0))
+
+def loss(params, inputs, targets):
+    """
+    Compute loss
+    """
+    preds = batched_predict(params, inputs)
+    
+    return -jnp.mean(preds * targets)
+
+@jit
+def update(params, x, y, learning_rate):
+    """
+    Perform backpropagation
+    """
+    grads = grad(loss)(params, x, y)
+
+    return [(w - learning_rate * dw, b - learning_rate * db) for (w, b), (dw, db) in zip(params, grads)]
+
+def accuracy(params, inputs, targets):
+    """
+    Test accuracy 
+    """
+
+    target_class = jnp.argmax(targets, axis=1)
+    predicted_class = jnp.argmax(batched_predict(params, inputs), axis=1)
+    
+    return jnp.mean(predicted_class == target_class)
+
+def train(params, ds, train_images, train_labels, test_images, test_labels, num_labels, num_pixels, learning_rate, epochs=10):
+
+    for epoch in range(epochs):
+        print(f"epoch: [{epoch}]")
+
+        start_time = time.time()
+        for x, y in ds:
+            x = jnp.reshape(x, (len(x), num_pixels))
+            y = one_hot_encoder(y, num_labels)
+        
+        params = update(params, x, y, learning_rate)
+        
+        epoch_time = time.time() - start_time
+
+        train_acc = accuracy(params, train_images, train_labels)
+        test_acc = accuracy(params, test_images, test_labels)
+        
+        print("Epoch {} in {:0.2f} sec".format(epoch, epoch_time))
+        print("Training set accuracy {}".format(train_acc))
+        print("Test set accuracy {}".format(test_acc))
