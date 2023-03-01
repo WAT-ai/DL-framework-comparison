bba5d99a67c8bb2f843ad88ce63e20c1e274371b
Trevor Yu <trevor.yu@uwaterloo.ca>
2023-03-01 05:55:59 +0000

    Add scripts to run tests

---
+++ multi-layer-perceptron/test_python_mlp.sh
@@ -0,0 +1,9 @@
+# 10 MLP trials
+declare -a seeds=(42 0 100 87 1234 7 10 13 17 19)
+declare -a frameworks=("jax" "pytorch" "tensorflow")
+
+for seed in ${seeds[@]}; do
+    for framework in ${frameworks[@]}; do
+        tsp python3 ${framework}_test.py --seed=$seed
+    done
+done
