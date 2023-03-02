97e1e4dfe8f3b1cebea2072e4de20bca780332d8
Trevor Yu <trevor.yu@uwaterloo.ca>
2023-03-02 02:56:27 +0000

    Add mxnet to frameworks

--- multi-layer-perceptron/test_python_mlp.sh
+++ multi-layer-perceptron/test_python_mlp.sh
@@ -1,6 +1,6 @@
 # 10 MLP trials
 declare -a seeds=(42 0 100 87 1234 7 10 13 17 19)
-declare -a frameworks=("jax" "pytorch" "tensorflow")
+declare -a frameworks=("jax" "mxnet" "pytorch" "tensorflow")
 
 for seed in ${seeds[@]}; do
     for framework in ${frameworks[@]}; do
